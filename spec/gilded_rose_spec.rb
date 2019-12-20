83c37c8489e4e97d3622364889315f27d28f3fe6
Tony Distinti <25881824+breadoliveoilsalt@users.noreply.github.com>
2019-12-20 14:31:07 -0500

    Uncomment conjured items tests

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -208,51 +208,51 @@ describe GildedRose do
 
     end
 
-  #   context "for Conjured items" do
+    context "for Conjured items" do
 
-  #     it "lowers the quality by 2 if the sell_in date is greater than 0" do 
-  #       conjured_item_1 = Item.new("Conjured Item", 10, 5)
-  #       conjured_item_2 = Item.new("Conjured Item", 1, 5)
-  #       items = [conjured_item_1, conjured_item_2]
+      it "lowers the quality by 2 if the sell_in date is greater than 0" do 
+        conjured_item_1 = Item.new("Conjured Item", 10, 5)
+        conjured_item_2 = Item.new("Conjured Item", 1, 5)
+        items = [conjured_item_1, conjured_item_2]
         
-  #       GildedRose.new(items).update_quality()
+        GildedRose.new(items).update_quality()
 
-  #       expect(conjured_item_1.quality).to eq 3
-  #       expect(conjured_item_2.quality).to eq 3
-  #     end
+        expect(conjured_item_1.quality).to eq 3
+        expect(conjured_item_2.quality).to eq 3
+      end
 
-  #     it "lowers the quality by 4 if the sell_in date is 0 or less" do
-  #       conjured_item_1 = Item.new("Conjured Item", 0, 5)
-  #       conjured_item_2 = Item.new("Conjured Item", -10, 5)
-  #       items = [conjured_item_1, conjured_item_2]
+      it "lowers the quality by 4 if the sell_in date is 0 or less" do
+        conjured_item_1 = Item.new("Conjured Item", 0, 5)
+        conjured_item_2 = Item.new("Conjured Item", -10, 5)
+        items = [conjured_item_1, conjured_item_2]
         
-  #       GildedRose.new(items).update_quality()
+        GildedRose.new(items).update_quality()
 
-  #       expect(conjured_item_1.quality).to eq 1
-  #       expect(conjured_item_2.quality).to eq 1
-  #     end
+        expect(conjured_item_1.quality).to eq 1
+        expect(conjured_item_2.quality).to eq 1
+      end
 
-#        it "does not lower the quality below 0" do 
-#         conjured_item_1 = Item.new("Conjured Item", 0, 0)
-#         conjured_item_2 = Item.new("Conjured Item", -10, 0)
-#         items = [conjured_item_1, conjured_item_2]
-#        
-#         GildedRose.new(items).update_quality()
-#
-#         expect(conjured_item_1.quality).to eq 0
-#         expect(conjured_item_2.quality).to eq 0
-#        end
+       it "does not lower the quality below 0" do 
+        conjured_item_1 = Item.new("Conjured Item", 0, 0)
+        conjured_item_2 = Item.new("Conjured Item", -10, 0)
+        items = [conjured_item_1, conjured_item_2]
+       
+        GildedRose.new(items).update_quality()
 
-  #     it "lowers the sell_in date by 1" do
-  #       conjured_item = Item.new("Conjured Item", 10, 5)
-  #       items = [conjured_item]
+        expect(conjured_item_1.quality).to eq 0
+        expect(conjured_item_2.quality).to eq 0
+       end
+
+      it "lowers the sell_in date by 1" do
+        conjured_item = Item.new("Conjured Item", 10, 5)
+        items = [conjured_item]
         
-  #       GildedRose.new(items).update_quality()
+        GildedRose.new(items).update_quality()
 
-  #       expect(conjured_item.sell_in).to eq 9
-  #     end
+        expect(conjured_item.sell_in).to eq 9
+      end
 
-  #   end
+    end
   
   end
 
