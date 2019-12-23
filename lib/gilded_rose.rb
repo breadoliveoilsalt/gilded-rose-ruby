28f05fb9ec2a77ea86cd412d49ec2fa3b01546a5
Tony Distinti <25881824+breadoliveoilsalt@users.noreply.github.com>
2019-12-23 10:36:26 -0500

    Delete UpdaterFactory from GildedRose class file

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -58,18 +58,3 @@ class GildedRose
     end
   end
 end
-
-class UpdaterFactory
-
-  def self.get_updater(item) 
-
-    case item.name
-    when "Sulfuras, Hand of Ragnaros"
-      SulfurasUpdater.new(item)
-    else
-      DefaultUpdater(item)
-    end
-
-  end
-
-end
