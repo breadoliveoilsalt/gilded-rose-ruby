ce2ecb341df5110ddd73f111eaf3feb10d5d606d
Tony Distinti <25881824+breadoliveoilsalt@users.noreply.github.com>
2019-12-23 12:22:22 -0500

    Add latest classes to UpdaterFactory with tests

--- lib/updater_factory.rb
+++ lib/updater_factory.rb
@@ -1,3 +1,9 @@
+require_relative './default_updater.rb'
+require_relative './sulfuras_updater.rb'
+require_relative './aged_brie_updater.rb'
+require_relative './backstage_pass_updater.rb'
+require_relative './conjured_item_updater.rb'
+
 class UpdaterFactory
 
   def self.get_updater(item) 
@@ -7,6 +13,10 @@ class UpdaterFactory
       SulfurasUpdater.new(item)
     when "Aged Brie"
       AgedBrieUpdater.new(item)
+    when "Backstage passes to a TAFKAL80ETC concert"
+      BackstagePassUpdater.new(item)
+    when "Conjured Item"
+      ConjuredItemUpdater.new(item)
     else
       DefaultUpdater.new(item)
     end
--- spec/updater_factory_spec.rb
+++ spec/updater_factory_spec.rb
@@ -1,7 +1,10 @@
 require_relative '../lib/item.rb'
 require_relative '../lib/updater_factory.rb'
-require_relative '../lib/sulfuras_updater.rb'
 require_relative '../lib/default_updater.rb'
+require_relative '../lib/sulfuras_updater.rb'
+require_relative '../lib/aged_brie_updater.rb'
+require_relative '../lib/backstage_pass_updater.rb'
+require_relative '../lib/conjured_item_updater.rb'
 
 describe UpdaterFactory do
 
@@ -13,6 +16,30 @@ describe UpdaterFactory do
       result = UpdaterFactory.get_updater(sulfuras)
 
       expect(result).to be_a(SulfurasUpdater)
+    end
+
+    it "instantiates a AgedBrieUpdater when passed an Aged Brie item" do
+      brie = Item.new("Aged Brie", 10, 10)
+
+      result = UpdaterFactory.get_updater(brie)
+
+      expect(result).to be_a(AgedBrieUpdater)
+    end
+
+    it "instantiates a BackstagePassUpdater when passed a Backstage Pass item" do
+      backstage_pass = Item.new("Backstage passes to a TAFKAL80ETC concert", 10, 10)
+
+      result = UpdaterFactory.get_updater(backstage_pass)
+
+      expect(result).to be_a(BackstagePassUpdater)
+    end
+
+    it "instantiates a ConjuredItemUpdater when passed a Conjured Item" do
+      conjured_item = Item.new("Conjured Item", 10, 10)
+
+      result = UpdaterFactory.get_updater(conjured_item)
+
+      expect(result).to be_a(ConjuredItemUpdater)
     end
 
     it "instantiates a DefaultUpdater when passed a non-specialized item" do
