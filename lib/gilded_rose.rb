ab6ca825d6066e06171386dc63aef92c804efad2
Tony Distinti <25881824+breadoliveoilsalt@users.noreply.github.com>
2019-12-23 12:28:15 -0500

    Incorporate UpdaterFactory into GildedRose

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,7 +1,4 @@
-require_relative './default_updater.rb'
-require_relative './sulfuras_updater.rb'
-require_relative './aged_brie_updater.rb'
-require_relative './backstage_pass_updater.rb'
+require_relative './updater_factory.rb'
 
 class GildedRose
 
@@ -11,39 +8,10 @@ class GildedRose
 
   def update_quality()
     @items.each do |item|
-        
-      if item.name == "Sulfuras, Hand of Ragnaros"
-        updater = SulfurasUpdater.new(item)
-        updater.update_item_quality
-        updater.update_item_sell_in
-        next
-      end
-
-      if item.name == "Aged Brie"
-        updater = AgedBrieUpdater.new(item)
-        updater.update_item_quality
-        updater.update_item_sell_in
-        next
-      end
-
-      if item.name == "Backstage passes to a TAFKAL80ETC concert"
-        updater = BackstagePassUpdater.new(item)
-        updater.update_item_quality
-        updater.update_item_sell_in
-        next
-      end
-
-      if item.name == "Conjured Item"
-        updater = ConjuredItemUpdater.new(item)
-        updater.update_item_quality
-        updater.update_item_sell_in
-        next
-      end
-
-      updater = DefaultUpdater.new(item)
+      updater = UpdaterFactory.get_updater(item)
       updater.update_item_quality
       updater.update_item_sell_in
-
     end
   end
+
 end
