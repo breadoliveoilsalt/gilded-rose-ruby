ce2824dbde6d605244001ac9f8d4ca105dca5051
Tony Distinti <25881824+breadoliveoilsalt@users.noreply.github.com>
2019-12-26 16:04:10 -0500

    Add #update to Item toward implementing Strategy pattern
    #update calls an update_stragegy. Also add setter for update_strategy

--- lib/item.rb
+++ lib/item.rb
@@ -5,9 +5,19 @@ class Item
     @name = name
     @sell_in = sell_in
     @quality = quality
+    @update_strategy = nil
   end
 
   def to_s()
     "#{@name}, #{@sell_in}, #{@quality}"
   end
+  
+  def update
+    @update_strategy.update
+  end
+
+  def set_update_strategy(update_strategy)
+    @update_stragegy = update_stragegy
+  end
+  
 end
