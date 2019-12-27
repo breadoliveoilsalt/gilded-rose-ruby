23a906550f8d87e696c3ef500e8d23237783b83e
Tony Distinti <25881824+breadoliveoilsalt@users.noreply.github.com>
2019-12-27 13:46:18 -0500

    Refactor tests for BackstagePassStrategy in light of strategy pattern

--- spec/backstage_pass_strategy_spec.rb
+++ spec/backstage_pass_strategy_spec.rb
@@ -1,24 +1,18 @@
 require_relative '../lib/item.rb'
-require_relative '../lib/backstage_pass_updater.rb'
+require_relative '../lib/item_update_strategies.rb'
 
-describe BackstagePassUpdater do
+describe ItemUpdateStrategies::BackstagePassStrategy do
 
-  it "requires an item when initialized" do
-
-    expect{BackstagePassUpdater.new}.to raise_error(ArgumentError)
-
-  end
-
-  describe "#update_item_quality" do
+  describe "#update" do
 
     it "increases the quality of 1 if the sell_in date is greater than 10" do
       backstagePass_1 = Item.new("Backstage passes to a TAFKAL80ETC concert", 20, 0)
-      updater_1 = BackstagePassUpdater.new(backstagePass_1)
+      updater_1 = ItemUpdateStrategies::BackstagePassStrategy.new
       backstagePass_2 = Item.new("Backstage passes to a TAFKAL80ETC concert", 30, 0)
-      updater_2 = BackstagePassUpdater.new(backstagePass_2)
+      updater_2 = ItemUpdateStrategies::BackstagePassStrategy.new
 
-      updater_1.update_item_quality 
-      updater_2.update_item_quality 
+      updater_1.update(backstagePass_1) 
+      updater_2.update(backstagePass_2) 
 
       expect(backstagePass_1.quality).to eq 1
       expect(backstagePass_2.quality).to eq 1
@@ -26,12 +20,12 @@ describe BackstagePassUpdater do
 
     it "increases the quality by 2 if the sell_in date is from 6 to 10" do
       backstagePass_1 = Item.new("Backstage passes to a TAFKAL80ETC concert", 10, 0)
-      updater_1 = BackstagePassUpdater.new(backstagePass_1)
+      updater_1 = ItemUpdateStrategies::BackstagePassStrategy.new
       backstagePass_2 = Item.new("Backstage passes to a TAFKAL80ETC concert", 6, 0)
-      updater_2 = BackstagePassUpdater.new(backstagePass_2)
+      updater_2 = ItemUpdateStrategies::BackstagePassStrategy.new
 
-      updater_1.update_item_quality 
-      updater_2.update_item_quality 
+      updater_1.update(backstagePass_1) 
+      updater_2.update(backstagePass_2) 
       
       expect(backstagePass_1.quality).to eq 2
       expect(backstagePass_2.quality).to eq 2
@@ -39,12 +33,12 @@ describe BackstagePassUpdater do
 
     it "increases the quality by 3 if the sell_in date is from 1 to 5" do
       backstagePass_1 = Item.new("Backstage passes to a TAFKAL80ETC concert", 1, 0)
-      updater_1 = BackstagePassUpdater.new(backstagePass_1)
+      updater_1 = ItemUpdateStrategies::BackstagePassStrategy.new
       backstagePass_2 = Item.new("Backstage passes to a TAFKAL80ETC concert", 5, 0)
-      updater_2 = BackstagePassUpdater.new(backstagePass_2)
+      updater_2 = ItemUpdateStrategies::BackstagePassStrategy.new
 
-      updater_1.update_item_quality 
-      updater_2.update_item_quality 
+      updater_1.update(backstagePass_1) 
+      updater_2.update(backstagePass_2) 
       
       expect(backstagePass_1.quality).to eq 3
       expect(backstagePass_2.quality).to eq 3
@@ -52,12 +46,12 @@ describe BackstagePassUpdater do
 
     it "does not increase the quality of a backstage pass beyond 50" do
       backstagePass_1 = Item.new("Backstage passes to a TAFKAL80ETC concert", 10, 50)
-      updater_1 = BackstagePassUpdater.new(backstagePass_1)
+      updater_1 = ItemUpdateStrategies::BackstagePassStrategy.new
       backstagePass_2 = Item.new("Backstage passes to a TAFKAL80ETC concert", 3, 49)
-      updater_2 = BackstagePassUpdater.new(backstagePass_2)
+      updater_2 = ItemUpdateStrategies::BackstagePassStrategy.new
 
-      updater_1.update_item_quality 
-      updater_2.update_item_quality 
+      updater_1.update(backstagePass_1) 
+      updater_2.update(backstagePass_2) 
       
       expect(backstagePass_1.quality).to eq 50
       expect(backstagePass_2.quality).to eq 50
@@ -65,12 +59,12 @@ describe BackstagePassUpdater do
 
     it "lowers the quality to 0 if the sell_in date is 0 or less" do
       backstagePass_1 = Item.new("Backstage passes to a TAFKAL80ETC concert", 0, 45)
-      updater_1 = BackstagePassUpdater.new(backstagePass_1)
+      updater_1 = ItemUpdateStrategies::BackstagePassStrategy.new
       backstagePass_2 = Item.new("Backstage passes to a TAFKAL80ETC concert", -1, 45)
-      updater_2 = BackstagePassUpdater.new(backstagePass_2)
+      updater_2 = ItemUpdateStrategies::BackstagePassStrategy.new
 
-      updater_1.update_item_quality 
-      updater_2.update_item_quality 
+      updater_1.update(backstagePass_1) 
+      updater_2.update(backstagePass_2) 
       
       expect(backstagePass_1.quality).to eq 0
       expect(backstagePass_2.quality).to eq 0
@@ -78,26 +72,22 @@ describe BackstagePassUpdater do
 
     it "does not decrease the quality below 0" do
       backstagePass_1 = Item.new("Backstage passes to a TAFKAL80ETC concert", 0, 0)
-      updater_1 = BackstagePassUpdater.new(backstagePass_1)
+      updater_1 = ItemUpdateStrategies::BackstagePassStrategy.new
       backstagePass_2 = Item.new("Backstage passes to a TAFKAL80ETC concert", -1, 0)
-      updater_2 = BackstagePassUpdater.new(backstagePass_2)
+      updater_2 = ItemUpdateStrategies::BackstagePassStrategy.new
 
-      updater_1.update_item_quality 
-      updater_2.update_item_quality 
+      updater_1.update(backstagePass_1) 
+      updater_2.update(backstagePass_2) 
       
       expect(backstagePass_1.quality).to eq 0
       expect(backstagePass_2.quality).to eq 0
     end
 
-  end      
-
-  describe "#update_item_sell_in" do
-    
     it "lowers the sell_in date by 1" do
       backstagePass = Item.new("Backstage passes to a TAFKAL80ETC concert", 10, 45)
-      updater = BackstagePassUpdater.new(backstagePass)
+      updater = ItemUpdateStrategies::BackstagePassStrategy.new
 
-      updater.update_item_sell_in
+      updater.update(backstagePass)
       
       expect(backstagePass.sell_in).to eq 9
     end
