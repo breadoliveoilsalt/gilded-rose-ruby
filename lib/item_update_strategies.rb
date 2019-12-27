aa7c33b13f3d0792cbbaa83f20b1ec136bca6c83
Tony Distinti <25881824+breadoliveoilsalt@users.noreply.github.com>
2019-12-27 14:36:57 -0500

    Refactor conditionals in ItemUpdateStrategies

--- lib/item_update_strategies.rb
+++ lib/item_update_strategies.rb
@@ -3,12 +3,10 @@ module ItemUpdateStrategies
   class DefaultStrategy
 
     def update(item)
-      if item.sell_in > 0
-        item.quality = [0, item.quality - 1].max
-      else
-        item.quality = [0, item.quality - 2].max
-      end
       item.sell_in = item.sell_in - 1
+      return if item.quality <= 0
+      item.quality = item.quality - 1 if item.quality > 0
+      item.quality = item.quality - 1 if item.quality > 0 && item.sell_in < 0
     end
 
   end
@@ -23,12 +21,10 @@ module ItemUpdateStrategies
   class AgedBrieStrategy
     
     def update(item)
-      if item.sell_in > 0
-        item.quality = [50, item.quality + 1].min
-      else
-        item.quality = [50, item.quality + 2].min
-      end
       item.sell_in = item.sell_in - 1
+      return if item.quality >= 50
+      item.quality = item.quality + 1 if item.quality < 50
+      item.quality = item.quality + 1 if item.quality < 50 && item.sell_in < 0
     end
 
   end
@@ -36,16 +32,16 @@ module ItemUpdateStrategies
   class BackstagePassStrategy
 
     def update(item)
-      if item.sell_in > 10
+      item.sell_in = item.sell_in - 1
+      if item.sell_in >= 10
         item.quality = [50, item.quality + 1].min
-      elsif item.sell_in > 5
+      elsif item.sell_in >= 5
         item.quality = [50, item.quality + 2].min
-      elsif item.sell_in > 0
+      elsif item.sell_in >= 0
         item.quality = [50, item.quality + 3].min
       else
         item.quality = 0
       end
-      item.sell_in = item.sell_in - 1
     end
 
   end
